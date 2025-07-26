048cd380103e9dad710efc34df4258be04aa79c5
Deividson Henry <109252541+DeividsonHenrique@users.noreply.github.com>
2025-07-25 22:32:46 -0300

    day 25.07.25

--- GameStore/src/Components/Footer/index.jsx
+++ GameStore/src/Components/Footer/index.jsx
@@ -1,12 +1,12 @@
-import { Container } from './style'
+import { Container } from "./style";
 
 function Footer() {
   return (
     <Container>
       <h3>Todos os direitos reservados</h3>
-        <p>Desenvolvido por Deivi</p>
+      <p>Desenvolvido por Deivi</p>
     </Container>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
